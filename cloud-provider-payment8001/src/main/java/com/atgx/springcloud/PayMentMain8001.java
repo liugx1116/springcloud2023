1e103cfedb5eb443a3f903968cd428e76f5be657
liugx <sunshine_lgx@163.com>
2023-02-23 18:04:01 +0800

    Initial commit 初始化maven父项目

---
+++ cloud-provider-payment8001/src/main/java/com/atgx/springcloud/PayMentMain8001.java
@@ -0,0 +1,18 @@
+package com.atgx.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: liugx
+ * @CreateTime: 2023-02-22  12:32
+ * @Description: 支付模块启动类
+ * @Version: 1.0
+ */
+@SpringBootApplication
+public class PayMentMain8001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayMentMain8001.class,args);
+    }
+}
---
+++ cloud-provider-payment8001/src/main/java/com/atgx/springcloud/entities/test1.java
@@ -0,0 +1,10 @@
+package com.atgx.springcloud.entities;
+
+/**
+ * @Author: liugx
+ * @CreateTime: 2023-02-22  12:29
+ * @Description: TODO
+ * @Version: 1.0
+ */
+public class test1 {
+}
